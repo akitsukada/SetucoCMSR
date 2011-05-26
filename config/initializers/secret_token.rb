fbcbceb415ae54132eb2e6ab6be32116009a453b
akitsukada <tsukada.akihiro@gmail.com>
2011-05-26 11:17:44 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SetucoCMSR::Application.config.secret_token = 'e18305eabed2b0d4de21e0457d06f48d43dae5dc1f6dfea7f8038abab5a70ab4a3201921712b1f185a53cb9ee04a406f43bbd4dd74efec7eba0341030f122f70'
