e770dac4aace55d678a6afb394b2d17b63550742
akitsukada <tsukada.akihiro@gmail.com>
2011-05-26 23:34:22 +0900

    install jQuery, add ja.yml and en-US.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :detect_locale
+
+  private
+  def detect_locale
+    I18n.locale = request.headers['Accept-Language'].scan(/^[a-z]{2}/).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SetucoCMSR::Application.routes.draw do
+  get "test/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
