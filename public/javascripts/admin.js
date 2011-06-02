13b4d5c52aedb24ae04c388a2f3bbcaa356867ce
akitsukada <tsukada.akihiro@gmail.com>
2011-06-02 15:45:54 +0900

    add hideNotice() to admin.js

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,3 +1,19 @@
+
+/********************
+ ** global functions
+ *********************/
+function hideNotice() {
+  $('div.section > div.messageArea', 'div#mainContentInner').css('display', 'none');
+}
+function hideErrorMessageArea() {
+  dojo.query('.errorMessageArea').style('display', 'none');
+}
+function confirmDeleteRedirect(url, item){
+  if (confirm("「" + item + "」を本当に削除してよろしいですか？")) {
+    location.href = url;
+  }
+}
+
 /*********************
  * category/index
  * tag/index
@@ -6,3 +22,5 @@ function toggleDisplay(idForHide, idForDisplay) {
   $("#" + idForHide).css('display','none');
   $("#" + idForDisplay).css('display','block');
 }
+
+
